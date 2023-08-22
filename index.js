d0ac4f0f908d5e37987365088f6120500b94ed2b
Abdur Rehman <abdurrehmansaleem5@gmail.com>
2023-08-22 03:32:50 -0700

    Update

--- index.js
+++ index.js
@@ -1,18 +1,37 @@
-let buffer ;
+let stream = [];
 // screen = document.getElementById("screen");
 buffer = document.getElementById("buffer");
 res = document.getElementById("res");
-const display = (value) =>{
- 
-    buffer.innerHTML = value;
- 
+function isNum(num){
+    return!isNaN(num);
 }
+function isOperator(operator){
+    return operator === "+" || operator === "-" || operator === "X" || operator === "/";
+}
+const display = (value) => {
+    if(isNum(value)){
+        buffer.innerHTML += value;
+    }
+    else if(isOperator(value)){
+        if(stream.length > 3){
+            
+        }
+        stream.push(buffer.innerHTML);
+        stream.push(value);
+        buffer.innerHTML = "";
+        res.innerHTML = stream;
+    }
+  //till function keys are presses build the numbers inputted into the steam
+  if (value === "=") {
+    stream.push(Number(buffer.innerHTML));
+    buffer.innerHTML = "";
+    res.innerHTML = stream;
+  }
+};
 
-document.querySelectorAll(".data").forEach((Element)=>{
-    Element.addEventListener("click",()=>{
-        display(Element.innerHTML);
-    })
-})
-document.addEventListener("keydown",(e)=>{
-    buffer.innerHTML = e.key;
-})
+document.querySelectorAll(".key").forEach((Element) => {
+  Element.addEventListener("click", () => {
+    display(Element.innerHTML);
+  });
+});
+
