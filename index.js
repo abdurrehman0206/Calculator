07c063358823867855cd70f472eeb2a4fe41ed6a
Abdur Rehman <abdurrehmansaleem5@gmail.com>
2023-08-22 23:39:26 +0500

    Final

--- index.js
+++ index.js
@@ -1,57 +1,24 @@
-let numbers = [];
-let operators = [];
-
-// screen = document.getElementById("screen");
-buffer = document.getElementById("buffer");
-res = document.getElementById("res");
-function isNum(num) {
-  return !isNaN(num);
-}
-function isOperator(operator) {
-  return (
-    operator === "+" || operator === "-" || operator === "X" || operator === "/"
-  );
-}
-const display = (value) => {
-  if (isNum(value)) {
-    buffer.innerHTML += value;
-  } else if (isOperator(value)) {
-    numbers.push(buffer.innerHTML);
-    buffer.innerHTML = "";
-    if (numbers.length === 2 && operators.length === 1) {
-        if (operators[0] === "+") {
-            res.innerHTML = Number(numbers[0]) + Number(numbers[1]);
-        } else if (operators[0] === "-") {
-            res.innerHTML = Number(numbers[0]) - Number(numbers[1]);
-        } else if (operators[0] === "X") {
-            res.innerHTML = Number(numbers[0]) * Number(numbers[1]);
-        } else if (operators[0] === "/") {
-            res.innerHTML = Number(numbers[0]) / Number(numbers[1]);
-        }
-        numbers = [];
-        numbers.push(res.innerHTML);
-        operators = [];
-        operators.push(value);
-    } else {
-      
-      operators.push(value);
-
-      res.innerHTML = numbers;
-    }
-  }
-
-  console.log(numbers.length, operators.length);
-
-  //till function keys are presses build the numbers inputted into the steam
-  if (value === "=") {
-    stream.push(Number(buffer.innerHTML));
-    buffer.innerHTML = "";
-    res.innerHTML = stream;
-  }
-};
-
+const res = document.getElementById("res");
+const clearButton = document.getElementById("clear-button");
+const deleteButton = document.getElementById("delete-button");
 document.querySelectorAll(".key").forEach((Element) => {
   Element.addEventListener("click", () => {
-    display(Element.innerHTML);
+    display(Element.value);
   });
 });
+
+clearButton.addEventListener("click", () => {
+  res.value = "";
+});
+deleteButton.addEventListener("click", () => {
+  res.value = res.value.slice(0, -1);
+});
+
+const display = (value) => {
+  if (value === "=") {
+    const result = eval(res.value);
+    res.value = result;
+  } else {
+    res.value += value;
+  }
+};
