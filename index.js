bf1196f083639c41116afcf48d4e9cae3e1ca2d3
Abdur Rehman <abdurrehmansaleem5@gmail.com>
2023-08-22 06:25:31 -0700

    Update

--- index.js
+++ index.js
@@ -1,26 +1,47 @@
-let stream = [];
+let numbers = [];
+let operators = [];
+
 // screen = document.getElementById("screen");
 buffer = document.getElementById("buffer");
 res = document.getElementById("res");
-function isNum(num){
-    return!isNaN(num);
+function isNum(num) {
+  return !isNaN(num);
 }
-function isOperator(operator){
-    return operator === "+" || operator === "-" || operator === "X" || operator === "/";
+function isOperator(operator) {
+  return (
+    operator === "+" || operator === "-" || operator === "X" || operator === "/"
+  );
 }
 const display = (value) => {
-    if(isNum(value)){
-        buffer.innerHTML += value;
-    }
-    else if(isOperator(value)){
-        if(stream.length > 3){
-            
+  if (isNum(value)) {
+    buffer.innerHTML += value;
+  } else if (isOperator(value)) {
+    numbers.push(buffer.innerHTML);
+    buffer.innerHTML = "";
+    if (numbers.length === 2 && operators.length === 1) {
+        if (operators[0] === "+") {
+            res.innerHTML = Number(numbers[0]) + Number(numbers[1]);
+        } else if (operators[0] === "-") {
+            res.innerHTML = Number(numbers[0]) - Number(numbers[1]);
+        } else if (operators[0] === "X") {
+            res.innerHTML = Number(numbers[0]) * Number(numbers[1]);
+        } else if (operators[0] === "/") {
+            res.innerHTML = Number(numbers[0]) / Number(numbers[1]);
         }
-        stream.push(buffer.innerHTML);
-        stream.push(value);
-        buffer.innerHTML = "";
-        res.innerHTML = stream;
+        numbers = [];
+        numbers.push(res.innerHTML);
+        operators = [];
+        operators.push(value);
+    } else {
+      
+      operators.push(value);
+
+      res.innerHTML = numbers;
     }
+  }
+
+  console.log(numbers.length, operators.length);
+
   //till function keys are presses build the numbers inputted into the steam
   if (value === "=") {
     stream.push(Number(buffer.innerHTML));
@@ -34,4 +55,3 @@ document.querySelectorAll(".key").forEach((Element) => {
     display(Element.innerHTML);
   });
 });
-
